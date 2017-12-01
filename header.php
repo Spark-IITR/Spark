7ed7890d6f61509883de56a108f20d12384aa78b
9prashant-1223 <prashantverma1223@gmail.com>
2017-12-02 03:11:45 +0530

    shift login to header

--- header.php
+++ header.php
@@ -1,4 +1,59 @@
-
+<?php
+  require_once 'config/config.php';
+ 
+  $username = $password = "";
+  $username_err = $password_err = "";
+ 
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+      if(empty(trim($_POST["username"]))){
+        $username_err = 'Please enter username.';
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    if(empty(trim($_POST['password']))){
+        $password_err = 'Please enter your password.';
+    } else{
+        $password = trim($_POST['password']);
+    }
+    
+    if(empty($username_err) && empty($password_err)){
+        $sql = "SELECT email, password FROM student WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($conn, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            $param_username = $username;
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            session_start();
+                            $_SESSION['username'] = $username;      
+                            header("location: contact.php");
+                        } else{
+                            $password_err = 'The password you entered was not valid.';
+                        }
+                    }else{echo 'error';}
+                } else{
+                    $username_err = 'No account found with that username.';
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        mysqli_stmt_close($stmt);
+    }
+    
+    mysqli_close($conn);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
