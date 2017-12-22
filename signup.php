b6f7d5f0dd9b26d07b0c1fe2005fc486ba8b3403
9prashant-1223 <prashantverma1223@gmail.com>
2017-12-22 16:42:58 +0530

    it
    added year cgpa department degree to signup'

--- signup.php
+++ signup.php
@@ -2,8 +2,8 @@
  <?php
 require 'config/config.php';
  
-$name = $username = $contact = $gender = $dob = $college = $password = $confirm_password = "";
-$name_err = $username_err = $contact_err = $gender_err = $dob_err = $college_err = $password_err = $confirm_password_err = "";
+$name = $username = $contact = $gender = $dob = $college = $password = $confirm_password = $department = $year = $cgpa = $degree = "";
+$name_err = $username_err = $contact_err = $gender_err = $dob_err = $college_err = $password_err = $confirm_password_err = $department_err = $year_err = $cgpa_err = $degree_err = "";
  
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
@@ -71,6 +71,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $college = trim($_POST['college']);
         // echo $college;
     }
+
+    if(empty(trim($_POST['department']))){
+        $department_err = "Please enter a department.";     
+    } else{
+        $department = trim($_POST['department']);
+        // echo $college;
+    }
+
+    if(empty(trim($_POST['year']))){
+        $year_err = "Please enter a year.";     
+    } else{
+        $year = trim($_POST['year']);
+        // echo $college;
+    }
+
+    if(empty(trim($_POST['cgpa']))){
+        $cgpa_err = "Please enter a C.G.P.A.";     
+    } else{
+        $cgpa = trim($_POST['cgpa']);
+        // echo $college;
+    }
+
+
     
 
     
@@ -93,10 +116,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     // echo $username_err; echo $password_err; echo $confirm_password_err;
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-         $sql = "INSERT INTO user (email, password, name, contact, dob, college, gender,role) VALUES (?, ?, ?, ?, ?, ?, ?,?)";
+         $sql = "INSERT INTO user (email, password, name, contact, dob, college, gender, role, department, cgpa, year, degree) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
          
         if($stmt = mysqli_prepare($conn, $sql)){
-            mysqli_stmt_bind_param($stmt, "ssssssss", $param_username, $param_password, $param_name, $param_contact, $param_dob, $param_college, $param_gender,$param_role);
+            mysqli_stmt_bind_param($stmt, "ssssssssssss", $param_username, $param_password, $param_name, $param_contact, $param_dob, $param_college, $param_gender, $param_role, $param_department, $param_cgpa, $param_year, $param_degree);
             // echo 'hello';
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); 
@@ -106,6 +129,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_college = $college;
             $param_gender = $gender;
             $param_role = "student";
+            $param_department = $department;
+            $param_cgpa = $cgpa;
+            $param_year = $year;
+            $param_degree = $degree;
             // echo $param_gender;
             // echo $param_username;
             if(mysqli_stmt_execute($stmt)){
@@ -115,91 +142,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
                 ?>
 
-                    <div class="container">
-            <div id="Error">
-                
-            </div>
-            <div class="row">
-                <div class="col-sm-8 col-sm-offset-2">
-                    <p class="signupHereTag">Sign Up Here ..</p>
-                    <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-           
-                       <div class="form-group   <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-                         <label for="" class="sr-only">Name<sup>*</sup></label>
-                         <div class="col-sm-12">
-                           <input type="text"  name="name"  class="form-control"  placeholder="Name" value="<?php echo $name; ?>" >
-                            <span class="help-block"><?php echo $name_err; ?></span>
-                         </div>
-                       </div>
-                        
-                       <div class="form-group  <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                         <label for="" class="sr-only">Email<sup>*</sup></label>
-                         <div class="col-sm-12">
-                           <input type="email" name="username"  class="form-control"  placeholder="Email"  value="<?php echo $username; ?>">
-                            <span class="help-block"><?php echo $username_err; ?></span>
-                         </div>
-                       </div>
-                      
-                       <div class="form-group   <?php echo (!empty($gender_err)) ? 'has-error' : ''; ?>">
-                         <label for="" class="sr-only">Gender</label>
-                         <div class="col-sm-6">
-                           <select class="form-control"  name="gender"  placeholder="gender">
-                              <option value="male">Male</option>
-                              <option value="female">Female</option>
-                           </select>
-                            <span class="help-block"><?php echo $gender_err; ?></span>
-                         </div>
-                        
-                         <label for="" class="sr-only">Date Of Birth</label>
-                         <div class="col-sm-6">
-                           <input type="text" name="dob"  class="form-control"  placeholder="Date Of Birth"  onfocus="(this.type='date')" onblur="(this.type='text')" value="<?php echo $dob; ?>" >
-                            <span class="help-block"> <?php echo $dob_err; ?></span>
-                         </div>
-                       </div>
-                       
-                       <div class="form-group   <?php echo (!empty($college_err)) ? 'has-error' : ''; ?>">
-                        <label for="" class="sr-only">College</label>
-                         <div class="col-sm-6">
-                           <input type="text" name="college"  class="form-control"  placeholder="College" value="<?php echo $college; ?>">
-                            <span class="help-block"><?php echo $college_err; ?></span>
-                         </div>
-                        
-                        <label for="" class="sr-only">Contact</label>
-                         <div class="col-sm-6">
-                           <input type="number"  name="contact" class="form-control"  placeholder="Contact" value="<?php echo $contact; ?>">
-                            <span class="help-block"><?php echo $contact_err; ?></span>
-                         </div>
-                       </div>
-                      
-                       <div class="form-group  <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                         <label for=""  class="sr-only">Password<sup>*</sup></label>
-                         <div class="col-sm-6">
-                           <input type="password" name="password" id="signupPassword" class="form-control"  placeholder="Password"  value="<?php echo $password; ?>" autocomplete="off">
-                            <span class="help-block"><?php echo $password_err; ?></span>
-                         </div>
-                        
-                         <label for="" class="sr-only">Confirm Password<sup>*</sup></label>
-                         <div class="col-sm-6">
-                           <input type="password" name="confirm_password" class="form-control" id="signupConfirmPassword" placeholder="Confirm Password"  value="<?php echo $confirm_password; ?>" autocomplete="off">
-                            <span class="help-block"><?php echo $confirm_password_err; ?></span>
-                         </div>
-                      
-                       </div>
-                       <div class="form-group">
-                         <div class="col-sm-offset-3 col-sm-6">
-                           <input type="submit" value="Sign In" class="btn btn-primary signupModalSignupButton" style="width: 100%;margin-bottom: 5vh" autocomplete="off">
-                         </div>
-                       </div>
-                     </form>
-
-                </div>
-            </div>
-        </div>
-
-           
-<?php require_once('footer.php'); ?>
-
-    <?php require_once('login_modal.php'); ?>
 
 
        <?php }
@@ -215,4 +157,118 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 
-		
+		<div class="container">
+            <div id="Error">
+                
+            </div>
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<p class="signupHereTag">Sign Up Here ..</p>
+					<form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+           
+			           <div class="form-group   <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+			             <label for="" class="sr-only">Name<sup>*</sup></label>
+			             <div class="col-sm-12">
+			               <input type="text"  name="name"  class="form-control"  placeholder="Name" value="<?php echo $name; ?>" >
+			                <span class="help-block"><?php echo $name_err; ?></span>
+			             </div>
+			           </div>
+			            
+			           <div class="form-group  <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+			             <label for="" class="sr-only">Email<sup>*</sup></label>
+			             <div class="col-sm-12">
+			               <input type="email" name="username"  class="form-control"  placeholder="Email"  value="<?php echo $username; ?>">
+			                <span class="help-block"><?php echo $username_err; ?></span>
+			             </div>
+			           </div>
+			          
+			           <div class="form-group   ">
+			             <label for="" class="sr-only">Gender</label>
+			             <div class="col-sm-6">
+			               <select class="form-control"  name="gender"  placeholder="gender">
+			                  <option value="male">Male</option>
+			                  <option value="female">Female</option>
+			               </select>
+			                <span class="help-block"><?php echo $gender_err; ?></span>
+			             </div>
+			            
+			             <label for="" class="sr-only">Date Of Birth</label>
+			             <div class="col-sm-6">
+			               <input type="text" name="dob"  class="form-control"  placeholder="Date Of Birth"  onfocus="(this.type='date')" onblur="(this.type='text')" value="<?php echo $dob; ?>" >
+			                <span class="help-block"> <?php echo $dob_err; ?></span>
+			             </div>
+			           </div>
+			           
+                       <div class="form-group  ">
+                        <label for="" class="sr-only">year</label>
+                         <div class="col-sm-6">
+                           <input type="text" name="year"  class="form-control"  placeholder="Year (1st)" value="<?php echo $year; ?>">
+                            <span class="help-block"><?php echo $year_err; ?></span>
+                         </div>
+                        
+                        <label for="" class="sr-only">degree</label>
+                         <div class="col-sm-6">
+                           <input type="text"  name="degree" class="form-control"  placeholder="Degree ( B.Tech )" value="<?php echo $degree; ?>">
+                            <span class="help-block"><?php echo $degree_err; ?></span>
+                         </div>
+                       </div>
+
+                       <div class="form-group  ">
+                        <label for="" class="sr-only">department</label>
+                         <div class="col-sm-6">
+                           <input type="text" name="department"  class="form-control"  placeholder="Department" value="<?php echo $department; ?>">
+                            <span class="help-block"><?php echo $department_err; ?></span>
+                         </div>
+                        
+                        <label for="" class="sr-only">cgpa</label>
+                         <div class="col-sm-6">
+                           <input type="text"  name="cgpa" class="form-control"  placeholder="C.G.P.A ( If % then convert it to C.G.P.A. ) " value="<?php echo $cgpa; ?>">
+                            <span class="help-block"><?php echo $cgpa_err; ?></span>
+                         </div>
+                       </div>
+
+                       
+
+			           <div class="form-group   <?php echo (!empty($college_err)) ? 'has-error' : ''; ?>">
+			            <label for="" class="sr-only">College</label>
+			             <div class="col-sm-6">
+			               <input type="text" name="college"  class="form-control"  placeholder="College" value="<?php echo $college; ?>">
+			                <span class="help-block"><?php echo $college_err; ?></span>
+			             </div>
+			            
+			            <label for="" class="sr-only">Contact</label>
+			             <div class="col-sm-6">
+			               <input type="number"  name="contact" class="form-control"  placeholder="Contact" value="<?php echo $contact; ?>">
+			                <span class="help-block"><?php echo $contact_err; ?></span>
+			             </div>
+			           </div>
+			          
+			           <div class="form-group  <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+			             <label for=""  class="sr-only">Password<sup>*</sup></label>
+			             <div class="col-sm-6">
+			               <input type="password" name="password" id="signupPassword" class="form-control"  placeholder="Password"  value="<?php echo $password; ?>" autocomplete="off">
+			                <span class="help-block"><?php echo $password_err; ?></span>
+			             </div>
+			            
+			             <label for="" class="sr-only">Confirm Password<sup>*</sup></label>
+			             <div class="col-sm-6">
+			               <input type="password" name="confirm_password" class="form-control" id="signupConfirmPassword" placeholder="Confirm Password"  value="<?php echo $confirm_password; ?>" autocomplete="off">
+			                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+			             </div>
+			          
+			           </div>
+			           <div class="form-group">
+			             <div class="col-sm-offset-3 col-sm-6">
+			               <input type="submit" value="Sign In" class="btn btn-primary signupModalSignupButton" style="width: 100%;margin-bottom: 5vh" autocomplete="off">
+			             </div>
+			           </div>
+			         </form>
+
+				</div>
+			</div>
+		</div>
+
+           
+<?php require_once('footer.php'); ?>
+
+    <?php require_once('login_modal.php'); ?>
