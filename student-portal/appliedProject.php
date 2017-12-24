6c7cc70793ad2f2a12356cc54299eb325cb5ce7f
9prashant-1223 <prashantverma1223@gmail.com>
2017-12-25 02:20:33 +0530

    stuudent 4th and 5th priority

--- student-portal/appliedProject.php
+++ student-portal/appliedProject.php
@@ -1,7 +1,6 @@
-			
-	<?php
-							/* fetch applied projects*/
-                        	 $sql0    = "select spriority1,spriority2,spriority3 from user where id=$studentRealId";
+
+									<?php 
+                        	 $sql0    = "select spriority1,spriority2,spriority3,spriority4,spriority5 from user where id=$studentRealId";
 							$result0 = $conn->query($sql0);
 
 							if(!$result0->num_rows == 0) {
@@ -9,10 +8,13 @@
 						       $spriority1FacultyId =  $row0['spriority1'];
 						       $spriority2FacultyId =  $row0['spriority2'];
 						       $spriority3FacultyId =  $row0['spriority3'];
+						       $spriority4FacultyId =  $row0['spriority4'];
+						       $spriority5FacultyId =  $row0['spriority5'];
+
 					             
-					             	/* fetch details of first priority */
-					              	$sql1    = "select id,name,department,project from user where id=$spriority1FacultyId";
-							      	$result1 = $conn->query($sql1);
+					             /* fetch details of first priority */
+					              $sql1    = "select id,name,department,project from user where id=$spriority1FacultyId";
+							      $result1 = $conn->query($sql1);
 									if(!$result1->num_rows == 0) {
 								    	$row1 = $result1->fetch_assoc();
 
@@ -24,7 +26,7 @@
 
 								    /* fetch details of second priority */
 								    $sql2    = "select id,name,department,project from user where id=$spriority2FacultyId";
-							      	$result2 = $conn->query($sql2);
+							      $result2 = $conn->query($sql2);
 									if(!$result2->num_rows == 0) {
 								    	$row2 = $result2->fetch_assoc();
 
@@ -36,7 +38,7 @@
 
 								    /* fetch details of third priority */
 								    $sql3    = "select id,name,department,project from user where id=$spriority3FacultyId";
-							      	$result3 = $conn->query($sql3);
+							      $result3 = $conn->query($sql3);
 									if(!$result3->num_rows == 0) {
 										$row3 = $result3->fetch_assoc();
 
@@ -48,7 +50,7 @@
 
 								    /* fetch details of third priority */
 								    $sql4    = "select id,name,department,project from user where id=$spriority4FacultyId";
-							      	$result4 = $conn->query($sql4);
+							      $result4 = $conn->query($sql4);
 									if(!$result4->num_rows == 0) {
 										$row4 = $result4->fetch_assoc();
 
@@ -60,7 +62,7 @@
 
 								    /* fetch details of third priority */
 								    $sql5    = "select id,name,department,project from user where id=$spriority5FacultyId";
-							      	$result5 = $conn->query($sql5);
+							      $result5 = $conn->query($sql5);
 									if(!$result5->num_rows == 0) {
 										$row5 = $result5->fetch_assoc();
 
@@ -69,48 +71,66 @@
 								    	$spriority5FacultyProject = $row5['project'];
 
 								    }
-							}
-						?>				
-
-
-
-				<table class="table table-striped">
-					<thead style="font-size: 14px;"><tr><th title="Field #1">Priority</th>
-                        <th title="Field #2">Name</th>
-                        <th title="Field #3">Department</th>
-                        <th title="Field #5">Tentative projects for summer internship</th>
-                    </tr></thead>
-                    <tbody id="myTable" ><tr>
-                        <td align="right">1st</td>
-                        <?php if(!$spriority1FacultyId){ ?>
-                        <td colspan="4"> <?php echo ' Project yet not choosen';?></td>
-                        <?php }else{ ?>
-                        
-                        <td><?php echo $spriority1FacultyName; ?></td>
-                        <td><?php echo $spriority1FacultyDepartment; ?></td>
-                        <td><?php echo $spriority1FacultyProject; ?></td>
-                      	<?php } ?>
-                    </tr>
-                    <tr>
-						<td align="right">2nd</td>
-                        <?php if(!$spriority2FacultyId){ ?>
-                        <td colspan="4"> <?php echo ' Project yet not choosen';?></td>
-                        <?php }else{ ?>
-                        
-                        <td><?php echo $spriority2FacultyName; ?></td>
-                        <td><?php echo $spriority2FacultyDepartment; ?></td>
-                        <td><?php echo $spriority2FacultyProject; ?></td>
-                      	<?php } ?>
-                    </tr>
-                    <tr>
-                        <td align="right">3rd</td>
-                        <?php if(!$spriority3FacultyId){ ?>
-                        <td colspan="4"> <?php echo 'Project yet not choosen';?></td>
-                        <?php }else{ ?>
-                        
-                        <td><?php echo $spriority3FacultyName; ?></td>
-                        <td><?php echo $spriority3FacultyDepartment; ?></td>
-                        <td><?php echo $spriority3FacultyProject; ?></td>
-                      	<?php } ?>
-                    </tr>
-				</table>
+							} ?>
+									<table class="table table-striped">
+										<thead style="font-size: 14px;"><tr><th title="Field #1">Priority</th>
+				                            <th title="Field #2">Name</th>
+				                            <th title="Field #3">Department</th>
+				                            <th title="Field #5">Tentative projects for summer internship</th>
+				                        </tr></thead>
+				                        <tbody id="myTable" ><tr>
+				                            <td align="right">1st</td>
+				                            <?php if(!$spriority1FacultyId){ ?>
+				                            <td colspan="4"> <?php echo ' Project yet not choosen';?></td>
+				                            <?php }else{ ?>
+				                            
+				                            <td><?php echo $spriority1FacultyName; ?></td>
+				                            <td><?php echo $spriority1FacultyDepartment; ?></td>
+				                            <td><?php echo $spriority1FacultyProject; ?></td>
+				                          	<?php } ?>
+				                        </tr>
+				                        <tr>
+											<td align="right">2nd</td>
+				                            <?php if(!$spriority2FacultyId){ ?>
+				                            <td colspan="4"> <?php echo ' Project yet not choosen';?></td>
+				                            <?php }else{ ?>
+				                            
+				                            <td><?php echo $spriority2FacultyName; ?></td>
+				                            <td><?php echo $spriority2FacultyDepartment; ?></td>
+				                            <td><?php echo $spriority2FacultyProject; ?></td>
+				                          	<?php } ?>
+				                        </tr>
+				                        <tr>
+				                            <td align="right">3rd</td>
+				                            <?php if(!$spriority3FacultyId){ ?>
+				                            <td colspan="4"> <?php echo 'Project yet not choosen';?></td>
+				                            <?php }else{ ?>
+				                            
+				                            <td><?php echo $spriority3FacultyName; ?></td>
+				                            <td><?php echo $spriority3FacultyDepartment; ?></td>
+				                            <td><?php echo $spriority3FacultyProject; ?></td>
+				                          	<?php } ?>
+				                        </tr>
+				                        <tr>
+				                            <td align="right">4th</td>
+				                            <?php if(!$spriority4FacultyId){ ?>
+				                            <td colspan="4"> <?php echo 'Project yet not choosen';?></td>
+				                            <?php }else{ ?>
+				                            
+				                            <td><?php echo $spriority4FacultyName; ?></td>
+				                            <td><?php echo $spriority4FacultyDepartment; ?></td>
+				                            <td><?php echo $spriority4FacultyProject; ?></td>
+				                          	<?php } ?>
+				                        </tr>
+				                        <tr>
+				                            <td align="right">5th</td>
+				                            <?php if(!$spriority5FacultyId){ ?>
+				                            <td colspan="4"> <?php echo 'Project yet not choosen';?></td>
+				                            <?php }else{ ?>
+				                            
+				                            <td><?php echo $spriority5FacultyName; ?></td>
+				                            <td><?php echo $spriority5FacultyDepartment; ?></td>
+				                            <td><?php echo $spriority5FacultyProject; ?></td>
+				                          	<?php } ?>
+				                        </tr>
+									</table>
--- student-portal/index.php
+++ student-portal/index.php
@@ -28,6 +28,7 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
 
 
                         	
+
 							 
 					
 ?>
@@ -96,15 +97,15 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
 						  	<div class="tab-content"  style="max-height: 50vh;overflow: scroll;">
 								<div role="tabpanel" class="tab-pane fade in" id="applied">
 									
-									<?php require_once('appliedProject.php'); ?>
-
+									<?php require_once('appliedProject.php');  ?>
+								
 								</div>
 
 
 								<div role="tabpanel" class="tab-pane fade in active" id="available">
 									
-									<?php require_once('availableProject.php'); ?>
-
+									<?php require_once('availableProject.php');  ?>
+									
 								</div>
 					      	</div>
 						</div>
@@ -134,11 +135,77 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
     				</div>
     			</div>
 
+<?php
+require '../config/config.php';
+
+if ($_POST && !empty($_FILES)) {
+    if($_FILES['resume']['error'] == 0) {
+        $email = $_POST['resumeId'];
+        $type = $conn->real_escape_string($_FILES['resume']['type']);
+        $data = $conn->real_escape_string(file_get_contents($_FILES  ['resume']['tmp_name']));
+        $size = intval($_FILES['resume']['size']);
+ 
+		 if ( in_array($type, array('application/pdf'))) {
+		     if ( $size < 500000) {
+
+		                $sql1 = "SELECT resume from user WHERE email= ? ";
+
+		        if($stmt1 = mysqli_prepare($conn, $sql1)){
+		            mysqli_stmt_bind_param($stmt1, "s", $param_username);
+		            
+		            $param_username = $email;
+		            
+		            if(mysqli_stmt_execute($stmt1)){
+		                mysqli_stmt_store_result($stmt1);
+
+		                if(!mysqli_stmt_num_rows($stmt1) == 0){                    
+		                    mysqli_stmt_bind_result($stmt1, $resumePdf);
+		                    if(mysqli_stmt_fetch($stmt1)){
+		                
+		                    	if($resumePdf==null){
+			                        $query = "UPDATE user set resume=? where email=?";
+			             			if($stmt3 = mysqli_prepare($conn, $query)){
+							            mysqli_stmt_bind_param($stmt3, "ss",$param_resume, $param_email);
+							            $param_email = $email;
+							            $param_resume = $data;
+							            if(mysqli_stmt_execute($stmt3)){
+							                 echo 'Resume uploaded';
+							                  // header ("location:../student-portal/");
+							            } else{
+							               echo 'Error! Failed to insert the file'
+			                               . "<pre>{$conn->error}</pre>";
+							            }
+							        }else {
+							        	echo 'Already uploaded';
+		                        		// header ("location:../student-portal/");
+		                        		// mysqli_stmt_close($stmt);
+							    	}
+		                    	}else{echo 'Already uploaded';
+		                        // header ("location:../student-portal/");
+		                		}
+		            		}
+		      			}
+		    		}
+		 		}
+			}
+			else{
+					echo "File size too large. Size limit is 100kb only.";
+			}
+		        
+		}
+			else{
+				echo "Choose pdf format.";
+		}
+	}
+        else {
+            echo 'File is not selected.';
+    }
+ 
+   mysqli_stmt_close($stmt1);
+}
 
 
-    			<?php require_once('../uploadFiles/uploadResume.php'); ?>
-
-
+?>
 
    <script>
    		function resume_error(){
