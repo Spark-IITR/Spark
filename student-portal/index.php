35252a44f628dec120e5f70199e56a36f2de026e
Pratham Gupta <prathamgupta567@gmail.com>
2017-12-26 03:38:08 +0530

    student-portal index.php updated
    finalized student-portal front-end

--- student-portal/index.php
+++ student-portal/index.php
@@ -39,15 +39,15 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
     			<div class="row">
     				 <div class="col-sm-12" style="text-align: center;">
 		    			<img src="../uploadFiles/showProfileImage.php?email=<?=$email ?>" class="studentProfileImg" alt="<?php echo $name; ?>">
-
+                         <p class="studentProfileName" ><strong><?php echo $name;?> </strong></p>
     				 </div>
     				 <div class="col-sm-12 col-xs-12">
-                         <p class="studentProfileUploadTag0" >Update Profile Picture</p>
+                         <div class="studentProfileUploadTag0" >Update Profile Picture</div>
     				 	<form action="../uploadFiles/imageUpload.php" method="post" enctype="multipart/form-data">
 					        <input type="hidden" name="imageId" value="<?php echo $email; ?>">
 					        <input type="hidden" name="imageRole" value="<?php echo $role; ?>">
 					        <div class="col-sm-7 col-xs-7"><input type="file" name="image" id="file" class="inputfile" />
-							<label for="file"><span class="glyphicon glyphicon-folder-open hidden-sm" style="padding-right: 7px;"></span>Select Image</label></div>
+							<label for="file"><span class="glyphicon glyphicon-folder-open hidden-sm selectImageButtonTabFix" style="padding-right: 7px;"></span>Select Image</label></div>
 	    					<div class="col-sm-5 col-xs-5"><input type="submit" name="submit" class="btn btn-default studentProfileImageSubmitButton inputfile1" value="Change" placeholder="" ></div>
 						</form>
     				 </div>
@@ -91,7 +91,7 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
 						  	<ul class="nav nav-tabs" role="tablist">
 							    <li role="presentation" class="active"><a href="#available" aria-controls="profile" role="tab" data-toggle="tab">Available Projects</a></li>
 							     <li role="presentation"><a href="#applied" aria-controls="home" role="tab" data-toggle="tab">Applied Projects</a></li>
-							     <li style="float: right;margin-top: -5vh"><input class="form-control projectSearchingInput" id="myInput" type="text" placeholder="Search Projects.."></li>
+							     <li style="float: right;margin-top: -5vh;" ><input class="form-control projectSearchingInput" id="myInput" type="text" placeholder="Search Projects.."></li>
 						  	</ul>
 
 						  	<div class="tab-content"  style="max-height: 50vh;overflow: scroll;">
@@ -123,14 +123,14 @@ $sql = "SELECT id,name,email,contact,department,college,recommendStatus,recommen
 
 						</form>
     				</div>
-    				<div class="col-sm-6 col-xs-12" style="display: block;">
+    				<div class="col-md-6 col-sm-6 col-xs-12" style=" display: block;">
                         <p class="studentProfileUploadTag" >Upload NOC/LOR</p>
 
     					<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
 					        <input type="hidden" name="nocId" value="<?php echo $email; ?>" />
-					        <div class="col-sm-7 col-xs-7"> <input type="file" name="noc" id="noc" class="inputfile" />
+					        <div class="col-md-7 col-sm-7 col-xs-7"> <input type="file" name="noc" id="noc" class="inputfile" />
 							<label for="noc"><span class="glyphicon glyphicon-folder-open" style="padding-right: 7px"></span></span>Select File</label></div>
-                            <div class="col-sm-5 col-xs-5"><input type="submit" name="submit" class="btn btn-default studentProfileImageSubmitButton inputfile1" value="Upload" placeholder="" ></div>
+                            <div class="col-md-5 col-sm-5 col-xs-5"><input type="submit" name="submit" class="btn btn-default studentProfileImageSubmitButton inputfile1" value="Upload" placeholder="" ></div>
 						</form>
     				</div>
     			</div>
@@ -216,55 +216,60 @@ if ($_POST && !empty($_FILES)) {
 		}
    </script> 			
 
-    			<div class="container-fluid" style="margin-top: 5vh">
-	    			<div class="alert alert-success" role="alert">
-						<ul>Instructions
-							<li>PLease Upload Your  NOC (for student of other than IITR) and Resume ( compulsory for all students ) otherwise your application will be rejected.</li>
-							<li>Size of Image / Resume / NOC should be less than 100 KB.</li>
-							<li>You will not be able to change the Resume / Image / NOC once uploaded . </li>
-							<li>Each student have three choices to select their project according to his/her priority.</li>
-							<li>Once the priorities are selected you will not be able to change it. So, choose priorities carefully after inspecting all the projects.</li>
-						</ul>
-				    </div>
-				</div>
 
-				<div class="container-fluid" style="margin-top: 5vh">
-	    			<div class="row">
-	    				<div class="col-sm-12">
-	    					<div class="jumbotron">
-							  <h2>Status</h2>
-							  <ul>
-
-							  	<?php if($recommendStatus==1){ ?>
-
-								  	<span style="font-weight: 800;color: green"><h3>Application Accepted</h1></span>
-
-
-								  	<?php	$query    = "select name from user where id=$recommendedFaculty";
-												$result5 = $conn->query($query);
-												if($result5) {
-												    if(!$result5->num_rows == 0) {
-												    	while($row5 = $result5->fetch_assoc()) {
-												        ?><li><span style="font-weight: 800">Faculty Recommended : </span> <?php echo $row5['name']; ?></li>
-												    <?php	}
-												    }
-												}  ?>
-								  	
-								  	<li><span style="font-weight: 800">Funding Type/Stipend : </span> <?php echo $fundingType; ?></li>
-								  	
-							  	<?php }else{ ?>
-
-							  	<span style="font-weight: 800;"><h3> Yet not recommended any faculty. </h1></span>
-
-							  	<?php } ?>
-
-							  </ul>
-							</div>
-	    				</div>
-	    			</div>
-	    		</div>
     		</div>
     	</div>
+
+        <div class="container-fluid" style="margin-top: 5vh;">
+            <div class="alert alert-success studentProfileInstructionBox " role="alert">
+                <ul><p class="studentProfileInstructionsTag"> <strong>Instructions :</strong> </p>
+                    <li>Please Upload Your  NOC (for student other than IITR) and Resume ( compulsory for all students ) otherwise your application will be rejected.</li>
+                    <li>Size of Image / Resume / NOC should be less than 100 KB.</li>
+                    <li>Resume / NOC must be in pdf format.</li>
+                    <li>Once uploaded, you will not be able to change the Resume / Image / NOC. </li>
+                    <li>Each student have three choices to select their project according to his/her priority.</li>
+                    <li>Once the priorities are selected you will not be able to change it. So, choose priorities carefully after inspecting all the projects.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container-fluid" style="margin-top: 5vh">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="alert alert-warning studentProfileStatusBox">
+                        <p class="studentProfileStatusTag"><strong>Status : </strong></p>
+                        <ul>
+
+                            <?php if($recommendStatus==1){ ?>
+
+                                <span style="font-weight: 800;color: green"><h3>Application Accepted</h1></span>
+
+
+                                <?php	$query    = "select name from user where id=$recommendedFaculty";
+                                $result5 = $conn->query($query);
+                                if($result5) {
+                                    if(!$result5->num_rows == 0) {
+                                        while($row5 = $result5->fetch_assoc()) {
+                                            ?><li><span style="font-weight: 800">Faculty Recommended : </span> <?php echo $row5['name']; ?></li>
+                                        <?php	}
+                                    }
+                                }  ?>
+
+                                <li><span style="font-weight: 800">Funding Type/Stipend : </span> <?php echo $fundingType; ?></li>
+
+                            <?php }else{ ?>
+
+                                <span class="studentProfileStatus"><h3 class="studentProfileStatus"> Yet not recommended any faculty. </h3></span>
+
+                            <?php } ?>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 <div style="height:5vh;"> </div>
     <?php require_once('../footer.php');?>
