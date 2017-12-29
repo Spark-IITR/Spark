2a53f3de635cf419f82fd635356434202d5c5e8c
9prashant-1223 <prashantverma1223@gmail.com>
2017-12-30 01:27:35 +0530

    choices of student done

--- student-portal/availableProject.php
+++ student-portal/availableProject.php
@@ -37,7 +37,9 @@
 																			<ul class='dropdown-menu'>
 																			    <li><a style='cursor:pointer'><span onclick='spriority1({$row11['id']})'>1st<span></a></li>
 																			    <li><a style='cursor:pointer'><span onclick='spriority2({$row11['id']})'>2nd<span></a></li>
-																			    
+																			    <li><a style='cursor:pointer'><span onclick='spriority3({$row11['id']})'>3rd<span></a></li>
+																			    <li><a style='cursor:pointer'><span onclick='spriority4({$row11['id']})'>4th<span></a></li>
+																			    <li><a style='cursor:pointer'><span onclick='spriority5({$row11['id']})'>5th<span></a></li>
 																			    
 																			</ul>
 																		</div>
@@ -54,22 +56,22 @@
 
 																var facultyId = data;
 																var studentId = <?php echo $studentRealId; ?> ;
-															 confirm = confirm('If you want to set 1st priority for your project to <?php echo $row11['name']; ?> ? ');
-															 if(confirm==true ){
-														 	 $.ajax({
-														        url: 'spriority1.php',
-														        data: {"studentId":studentId,"facultyId":facultyId},
-														        async: true,
-																type: 'POST',          
+															 	con = confirm('If you want to set 1st priority for your project to <?php echo $row11['name']; ?> ? ');
+															 	if(con==true ){
+																 	$.ajax({
+																        url: 'spriority1.php',
+																        data: {"studentId":studentId,"facultyId":facultyId},
+																        async: false,
+																		type: 'POST',          
 
-																success: function(data){
-																	$("#spriority1Div").html(data);
-														     },
-														       error : function(XMLHttpRequest, textStatus, errorThrown) {
-														            alert(errorThrown+ 'Priority Set.');
-														        }
-															    });
-															}
+																		success: function(data){
+																			$("#spriority1Div").html(data);
+																    	},
+																       error : function(XMLHttpRequest, textStatus, errorThrown) {
+																            alert(errorThrown+ 'Priority Set.');
+																        }
+																	});
+																}
 															}
 
 
@@ -77,60 +79,92 @@
 
 																var facultyId = data;
 																var studentId = <?php echo $studentRealId; ?> ;
-															 confirm = confirm('If you want to set 2nd priority for your project to <?php echo $row11['name']; ?> ? ');
-															 if(confirm==true ){
-														 	 $.ajax({
-														        url: 'spriority2.php',
-														        data: {"studentId":studentId,"facultyId":facultyId},
-														        async: true,
-																type: 'POST',          
+															 	con = confirm('If you want to set 2nd priority for your project to <?php echo $row11['name']; ?> ? ');
+																if(con==true ){
+																 	$.ajax({
+																        url: 'spriority2.php',
+																        data: {"studentId":studentId,"facultyId":facultyId},
+																        async: true,
+																		type: 'POST',          
 
-																success: function(data){
-																	$("#spriority1Div").html(data);
-														     },
-														       error : function(XMLHttpRequest, textStatus, errorThrown) {
-														            alert(errorThrown+ 'Priority Set.');
-														        }
-															    });
+																		success: function(data){
+																				$("#spriority1Div").html(data);
+																	    },
+																	    error : function(XMLHttpRequest, textStatus, errorThrown) {
+																	            alert(errorThrown+ 'Priority Set.');
+																	    }
+																	});
+																}
 															}
+
+															function spriority3(data){
+
+																var facultyId = data;
+																var studentId = <?php echo $studentRealId; ?> ;
+															 	con = confirm('If you want to set 3rd priority for your project to <?php echo $row11['name']; ?> ? ');
+																if(con==true ){
+																 	$.ajax({
+																        url: 'spriority3.php',
+																        data: {"studentId":studentId,"facultyId":facultyId},
+																        async: true,
+																		type: 'POST',          
+
+																		success: function(data){
+																				$("#spriority1Div").html(data);
+																	    },
+																	    error : function(XMLHttpRequest, textStatus, errorThrown) {
+																	            alert(errorThrown+ 'Priority Set.');
+																	    }
+																	});
+																}
+															}
+
+															function spriority4(data){
+
+																var facultyId = data;
+																var studentId = <?php echo $studentRealId; ?> ;
+															 	con = confirm('If you want to set 4th priority for your project to <?php echo $row11['name']; ?> ? ');
+																if(con==true ){
+																 	$.ajax({
+																        url: 'spriority4.php',
+																        data: {"studentId":studentId,"facultyId":facultyId},
+																        async: true,
+																		type: 'POST',          
+
+																		success: function(data){
+																				$("#spriority1Div").html(data);
+																	    },
+																	    error : function(XMLHttpRequest, textStatus, errorThrown) {
+																	            alert(errorThrown+ 'Priority Set.');
+																	    }
+																	});
+																}
+															}
+
+															function spriority5(data){
+
+																var facultyId = data;
+																var studentId = <?php echo $studentRealId; ?> ;
+															 	con = confirm('If you want to set 5th priority for your project to <?php echo $row11['name']; ?> ? ');
+																if(con==true ){
+																 	$.ajax({
+																        url: 'spriority5.php',
+																        data: {"studentId":studentId,"facultyId":facultyId},
+																        async: true,
+																		type: 'POST',          
+
+																		success: function(data){
+																				$("#spriority1Div").html(data);
+																	    },
+																	    error : function(XMLHttpRequest, textStatus, errorThrown) {
+																	            alert(errorThrown+ 'Priority Set.');
+																	    }
+																	});
+																}
 															}
 
 														</script>
 
-														<script>
-
-														$(function() {
-															$('#spriority2Button{$row11['id']}').click(function() {	
-															 var data = $('#spriority2Form{$row11['id']}').serialize();
-															 //alert(data);
-															 confirm = confirm('If you want to set 2nd priority for your project to {$row11['id']} ? ');
-															 if(confirm==true ){
-														 	 $.ajax({
-														        url: 'spriority2.php',
-														        data: data,
-														        async: true,
-																type: 'POST',          
-
-																success: function(data){
-																	//alert(data);
-
-																	if(data=='already present'){
-																		alert('Priority already choosen.');
-																	}
-																	if(data=='error'){
-																		alert('There is some internal error.');
-																	}
-														    		
-														     },
-														       error : function(XMLHttpRequest, textStatus, errorThrown) {
-														            alert('Priority Set.');
-														        }
-															    });
-															}
-															});
-														});
-
-														</script>
 
 											                 <?php
 											        }
---
+++ student-portal/spriority5.php
@@ -0,0 +1,50 @@
+<?php 
+require_once '../config/config.php';
+	$facultyId = $_POST['facultyId'];
+	$studentId  = $_POST['studentId'];
+
+	$sql100 = "SELECT spriority5 from user where id=?";
+				if($stmt1 = mysqli_prepare($conn, $sql100)){
+		            mysqli_stmt_bind_param($stmt1, "i", $param_id);
+		            
+		            $param_id = $studentId;
+		            
+		            if(mysqli_stmt_execute($stmt1)){
+		                mysqli_stmt_store_result($stmt1);
+
+		                if(!mysqli_stmt_num_rows($stmt1) == 0){                    
+		                    mysqli_stmt_bind_result($stmt1, $spriority5);
+		                    if(mysqli_stmt_fetch($stmt1)){
+	
+								if($spriority5==NULL || $spriority5==0){
+								$sql101 = "UPDATE user set spriority5=? where id=?";
+         
+							        if($stmt = mysqli_prepare($conn, $sql101)){
+							            mysqli_stmt_bind_param($stmt, "ii",$param_facultyId, $param_studentId);
+							            $param_facultyId = $facultyId;
+							            $param_studentId = $studentId;
+							            if(mysqli_stmt_execute($stmt)){
+							                echo '<script>alert("1st Priority set.")</script>';
+							            } else{
+							                echo '<script>alert("Something Went Wrong.")</script>';
+							            }
+							        }else {echo '<script>alert("Something Went Wrong.")</script>';}
+							         
+							        mysqli_stmt_close($stmt);
+							        
+							    }else{
+						    	echo '<script>alert("Already Choosen.")</script>';
+						    	}
+						    }
+
+						    
+						}
+					}mysqli_stmt_close($stmt1);
+				}
+			
+
+
+
+            // mysqli_close($conn);
+
+?>
