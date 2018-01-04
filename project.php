096319b63c798cddfc6274c40d1cee27cce39fb5
9prashant-1223 <prashantverma1223@gmail.com>
2018-01-04 17:05:57 +0530

    fetch all project

---
+++ project.php
@@ -0,0 +1,76 @@
+<?php include('header.php'); 
+require 'config/config.php';
+?>
+<body>
+
+        <div class="container-fluid">
+            <div class="row" id="aboutUs">
+                <div class="col-sm-7 col-sm-offset-1">
+                    <p class="projectsAvailableTag">PROJECTS AVAILABLE</p>
+                    <!-- <p class="text-justify"><strong>The following projects are available to work upon as summer intern during summer vacations, 2018 </strong></p> -->
+                </div>
+                <div class="col-sm-3">
+                    <input class="form-control projectSearchingInput" id="myInput" type="text" placeholder="Search Projects..">
+                </div>
+            </div>
+            <div class="row" id="objectives">
+                <div class="container" >
+                    <table class="table table-striped">
+                        <?php  
+                            $sql    = "SELECT sparkId,name,department,project from faculty where role='faculty'";
+                            $result = $conn->query($sql);
+                            if($result) {
+                                if($result->num_rows == 0) {
+                                    echo '<p>There are no entries in the database</p>';
+                                }
+                                else {
+                                    echo '<thead style="">
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Department</th>
+                                                <th>Research interests/Tentative projects for summer internship</th>
+                                            </tr>
+                                        </thead>';
+                                             
+                                    while($row = $result->fetch_assoc()) {
+                                        echo "
+                                            <tbody id='myTable'>
+                                                <tr>
+                                                    <td>{$row['sparkId']}</td>
+                                                    <td>{$row['name']}</td>
+                                                    <td>{$row['department']}</td>
+                                                    <td>{$row['project']}</td>
+                                                    <td></td>
+                                                </tr>
+                                                <div id='spriority1Div'></div>
+                                            </tbody>
+                                        
+                                        ";
+                                    }
+                                }
+                             $result->free();
+                            }  ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+
+<?php include 'footer.php' ; ?>
+
+    <script>
+        $(document).ready(function(){
+            $("#myInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#myTable tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+
+    <!--http://www.convertcsv.com/csv-to-html.htm-->
+
+</body>
