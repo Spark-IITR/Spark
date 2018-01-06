06a21e68929b1833f7ccb94d6ba615d17088ea33
9prashant-1223 <prashantverma1223@gmail.com>
2018-01-07 02:37:01 +0530

    combined csv with spriority faculty names

--- admin-portal/exportCsv.php
+++ admin-portal/exportCsv.php
@@ -8,11 +8,11 @@ require_once('../config/config.php');
 header('Content-Type:text/cs; charset=utf-8');
 header('Content-Disposition: attachment;filename=test.csv');
 
-	$output = fopen("php://output","w");
+$output = fopen("php://output","w");
 
 	fputcsv($output, array('ID','Spark ID','Name','Email','C.G.P.A','Department','Year','Degree','College','Gender','Contact','Student 1st Priority','Student 2nd Priority','Student 3rd Priority','Student 4th Priority','Student 5th Priority5','Recommended faculty','Funding Type'));
 
-	$query = "SELECT id,sparkId,name,email,cgpa,department,year,degree,college,gender,contact,spriority1,spriority2,spriority3,spriority4,spriority5,recommendedFaculty,fundingType from user where role='student' and ( spriority1!=0 or spriority2!=0 or spriority3!=0 or spriority4!=0 or spriority5!=0 )";
+	$query = "SELECT distinct c.id,c.sparkId,c.name,c.email,c.cgpa,c.department,c.year,c.degree,c.college,c.gender,c.contact, (select name from faculty as t1 where t1.id=c.spriority1) as spriority1,(select name from faculty as t2 where t2.id=c.spriority2) as spriority2,(select name from faculty as t3 where t3.id=c.spriority3) as spriority3,(select name from faculty as t4 where t4.id=c.spriority4) as spriority4,(select name from faculty as t5 where t5.id=c.spriority5) as spriority5,(select name from faculty as t6 where t6.id=c.recommendedFaculty) as recommendedFaculty,c.fundingType from student as c where role='student' and ( spriority1!=0 or spriority2!=0 or spriority3!=0 or spriority4!=0 or spriority5!=0 );";
 
 	$result = mysqli_query($conn,$query);
 
@@ -21,35 +21,6 @@ header('Content-Disposition: attachment;filename=test.csv');
 		}
 
 		fclose($output);
-
-		// 
-
-
-		header('Content-Type:text/cs; charset=utf-8');
-		header('Content-Disposition: attachment;filename=test2.csv');
-
-		$output1 = fopen("php://output1","w");
-
-	fputcsv($output1, array('Student 1st Priority','Student 2nd Priority','Student 3rd Priority','Student 4th Priority','Student 5th Priority5'));
-
-	$query1 = "SELECT faculty.name from student inner join faculty on student.spriority1 = faculty.id";
-
-	$result1 = mysqli_query($conn,$query1);
-
-		while($row1 = mysqli_fetch_assoc($result1)){
-			fputcsv($output1, $row1);
-		}
-
-		fclose($output1);
-
-
-header('Content-Type:text/cs; charset=utf-8');
-		header('Content-Disposition: attachment;filename=final.csv');
-
-		file_put_contents('final.csv',
-    file_get_contents('test.csv') .
-    file_get_contents('test2.csv')
-);
 
 mysqli_close($conn);
 
