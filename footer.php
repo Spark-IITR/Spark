1b9e0753c35367446f8ca3f04bb508f7562aadc5
9prashant-1223 <prashantverma1223@gmail.com>
2018-01-26 03:34:42 +0530

    footer set with icon

--- footer.php
+++ footer.php
@@ -5,72 +5,60 @@
             <div class="row" style="align-content: center;">
                 <div class="col-lg-1  col-md-1 hidden-sm hidden-xs">
                 </div>
-                <div class="col-lg-3  col-md-3 col-sm-4 col-xs-6  indexContactUs" >
+                <div class="col-lg-3  col-md-2 col-sm-4 col-xs-6  indexContactUs" >
                     <h3> Portals </h3>
                     <ul>
                         <li> <a href="https://www.iitr.ac.in/Main/pages/_en_Indian_Institute_of_Technology_Roorkee__en_.html"> IIT Roorkee Official Website </a> </li>
                         <li> <a href="https://www.iitr.ac.in/institute/pages/The_Institute.html"> About IIT Roorkee  </a> </li>
-                        <li> <a href="https://www.iitr.ac.in/campus_life/pages/index.html"> Campus Life @ IITR </a> </li>
-                        <li> <a href="https://www.iitr.ac.in/administration/pages/Institute_Central_Administration.html"> Administration IITR </a> </li>
-                        <li> <a href="https://www.iitr.ac.in/institute/pages/How_to_reach_IIT_Roorkee.html"> Reach IITR </a> </li>
+                        <li> <a href="https://www.iitr.ac.in/campus_life/pages/index.html"> Campus Life @ IIT Roorkee </a> </li>
+                        <li> <a href="https://www.iitr.ac.in/institute/pages/How_to_reach_IIT_Roorkee.html"> Reach IIT Roorkee </a> </li>
                     </ul>
                 </div>
-                <div class="col-lg-3  col-md-3 col-sm-4 col-xs-6  indexContactUs">
+                <div class="col-lg-3  col-md-3 col-sm-4 col-xs-6 col-sm-offset-1 indexContactUs">
                     <h3> Academics </h3>
                     <ul>
-                        <li> <a href="https://www.iitr.ac.in/Main/pages/_en_Departments__en_.html"> Departments of IIT Roorkee  </a> </li>
+                        <li> <a href="https://www.iitr.ac.in/administration/pages/Institute_Central_Administration.html"> Administration IITR </a> </li>
+                        <li> <a href="https://www.iitr.ac.in/Main/pages/_en_Departments__en_.html"> Departments of IITR  </a> </li>
                         <li> <a href="https://www.iitr.ac.in/Main/pages/_en_Centers__en_.html"> Centres @ IIT Roorkee </a> </li>
-                        <li> <a href="https://www.iitr.ac.in/administration/pages/Administration+DOFA+DOFA.html"> Faculty @ IITR </a> </li>
-                        <li> <a href="<?php echo base_url; ?>logout.php"> Logout </a> </li>
-
+                        <li> <a href="https://www.iitr.ac.in/administration/pages/Administration+DOFA+DOFA.html"> Faculty @ IIT Roorkee </a> </li>
                     </ul>
                 </div>
-
-
-                <div class="col-lg-4  col-md-5 col-sm-4 col-xs-8  indexContactUs indexContactUs1">
+                <div class="col-lg-3  col-md-3 col-sm-4 col-xs-8 col-sm-offset-1  indexContactUs indexContactUs1" style="margin-left: 6vw">
                     <h3> Contact Us </h3>
                     <ul>
                         <li>
                             <div class="input-append  text-left indexContactUs1Details" style="color: #000">
                                 <p><strong>For Programme related issues:</strong></p>
                                 <ul style="margin-top: 0px">
-                                    <li><a href="https://www.iitr.ac.in/departments/CH/pages/People+Prateek_Jha.html" style="color: blue">Dr. Prateek Kumar Jha</a></li>
-                                    <li>Assistant Professor</li>
-                                    <li><a href="https://www.iitr.ac.in/departments/CH/pages/index.html" >Department of Chemical Engineering,IIT Roorkee</a></li>
-                                    <li>Telephone: +91-1332-284810 (O)</li>
+                                    <li>Email at : spark@iitr.ac.in</li>
                                 </ul>
                             </div>
                         </li>
                     </ul>
 
+                    <ul class="social">
+                        <li>
+                            <a href="https://www.facebook.com/SPARK-IIT-Roorkee-1938628919796672/"> <i class="fa fa-facebook"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="social" style="margin-left: 4vw">
+                        <li>
+                            <a href="#"> <i class="fa fa-google-plus"></i></a>
+                        </li>
+                    </ul>
                 </div>
-
+                <div class="">
+                    <div class="pull-right">
+                        
+                    </div> 
+                </div>
             </div>
         </div>
     </div>
 
     <div class="footer-bottom">
         <div class="container">
-            <p class="pull-left"> Copyright © 2017, All Rights Reserved, Indian Institute of Technology, Roorkee </p>
-            <div class="pull-right">
-                <ul class="social">
-                    <li> <a href="https://www.facebook.com/SPARK-IIT-Roorkee-1938628919796672/"> <i class="fa fa-facebook">   </i> </a> </li>
-                </ul>
-            </div>
+            <p style="font-size: 11px;text-align: center;"> Copyright © 2017, All Rights Reserved, IIT Roorkee </p>
         </div>
     </div>
-    <!--/.footer-bottom-->
 </footer>
-
-
-
-
-<!-- <div>
-    <footer class="container-fluid footerBottom text-center">
-        <p>For Technical Assistance Contact:  Prashant Verma (+91-9919431223) Pratham Gupta (+91-8126119585)</p>
-    </footer>
-</div>
- -->
-
-         
-
