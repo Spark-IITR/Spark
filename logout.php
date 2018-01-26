1dc0ed46fbaf6790340e0fb8aa6a844e2db0c049
pv-912 <prashantverma1223@gmail.com>
2018-01-26 22:38:41 +0530

    merge conflict solved

--- logout.php
+++ logout.php
@@ -8,6 +8,6 @@ setcookie("role", "", time()-3600);
 setcookie("name", "", time()-3600); 
 session_destroy();
 // ob_end_flush();/
-header("location: http://localhost/spark");
+header("location: http://spark.iitr.ac.in");
 exit;
 ?>
